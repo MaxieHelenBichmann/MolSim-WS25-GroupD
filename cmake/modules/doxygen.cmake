7d11c89f99aa18d53200481a4a4b256d31724b7e
Henry Meyran <henry.meyran@tum.de>
2025-10-18 15:27:36 +0200

    added doxygen support

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,14 @@
-# make doc_doxygen optional if someone does not have / like doxygen
+find_package(Doxygen)
 
-# TODO: create CMake build option for the target.
+if(DOXYGEN_FOUND)
+    add_custom_target(doxygen
+        COMMAND ${DOXYGEN_EXECUTABLE} Doxyfile
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        COMMENT "Generating documentation with Doxygen"
+        VERBATIM
+    )
+    message(STATUS "Doxygen target created. Build it with 'make doxygen'.")
+else()
+    message(WARNING "Doxygen not found. The 'doxygen' target will not be available.")
+endif()
 
-# TODO: Add a custom target for building the documentation.
