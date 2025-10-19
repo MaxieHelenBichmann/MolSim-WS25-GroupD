a78afbd4626ffb7ae7c54036f8cc955961b03d9b
Henry Meyran <henry.meyran@tum.de>
2025-10-19 17:52:06 +0200

    fixed clang-tidy integration

--- cmake/modules/clang-tidy.cmake
+++ cmake/modules/clang-tidy.cmake
@@ -5,8 +5,7 @@ if(ENABLE_CLANG_TIDY)
     
     find_program(CLANG_TIDY_EXE clang-tidy)
     if(CLANG_TIDY_EXE)
-
-        #For other options like automatic fixes add -fix at the end
+        #For other options like automatic fixes add --fix at the end
         set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} )
         message(STATUS "clang-tidy found and enabled: ${CLANG_TIDY_EXE}")
     else ()
