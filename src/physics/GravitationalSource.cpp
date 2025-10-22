25a61416bd5b830d19e610e5a07926dd21df6709
Henry Meyran <henry.meyran@tum.de>
2025-10-22 08:03:36 +0200

    integration of new force calc in main

--- src/physics/GravitationalSource.cpp
+++ src/physics/GravitationalSource.cpp
@@ -1,8 +1,9 @@
 #include "GravitationalForce.h"
+#include "utils/ArrayUtils.h"
 
 std::array<double, 3> GravitationalForce::calculateForce(const Particle& p1, const Particle& p2) const {
-  const std::array<double, 3> diff = {p2.getX()[0] - p1.getX()[0], p2.getX()[1] - p1.getX()[1],
-                                      p2.getX()[2] - p1.getX()[2]};
-  double coeff = p1.getM() * p2.getM() / std::pow(ArrayUtils::L2Norm<std::array<double, 3>>(diff), 3);
-  return {p1.getF()[0] - coeff * diff[0], p1.getF()[1] - coeff * diff[1], p1.getF()[2] - coeff * diff[2]};
+  const auto difference = p2.getX() - p1.getX();
+  const double distance = difference.euclidNorm();
+  const double coeff = p1.getM() * p2.getM() / std::pow(distance, 3);
+  return coeff * difference;
 }
