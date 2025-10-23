411d114e61aa15804afff0cf15efce11a2c75500
Maxie Bichmann <maxie.bichmann@tum.de>
2025-10-23 18:36:30 +0200

    add documentation to SimpleContainer

--- src/particles/container/SimpleContainer.h
+++ src/particles/container/SimpleContainer.h
@@ -6,15 +6,38 @@
 
 namespace particle_containers {
 
+/**
+ * @brief Simple Container for Particles
+ *
+ * This container implements the concept ParticleContainer.
+ * It essentially uses a simple std::vector<Particle> to store the Particles.
+ * Thus it is helpful for it to inherit all members (and constructors) directly from std::vector, immediately fulfilling
+ * large part of the ParticleContainer concept. It implements the remaining functions for adding a Particle.
+ *
+ */
 class SimpleContainer : public std::vector<Particle> {
  public:
   // constructors
   using std::vector<Particle>::vector;
 
-  // modify
+  /**
+   * @brief Adding an already existing Particle to the SimpleContainer by copying it.
+   */
   void addParticle(Particle&& value);
+
+  /**
+   * @brief Adding an already existing const Particle to the SimpleContainer by copying it.
+   */
   void addParticle(const Particle& value);
+
+  /**
+   * @brief Directly constructing a Particle with its required parameters in-place.
+   */
   void addParticle(Vector<double, 3> x_arg, Vector<double, 3> v_arg, double m_arg);
+
+  /**
+   * @brief Directly constructing a Particle with its required parameters, including type, in-place.
+   */
   void addParticle(Vector<double, 3> x_arg, Vector<double, 3> v_arg, double m_arg, int type);
 };
 }  // namespace particle_containers
