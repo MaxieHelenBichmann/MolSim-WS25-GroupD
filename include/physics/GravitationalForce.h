670b4bfd94849c6fb65f28459cb4c744eb5dce6f
Henry Meyran <henry.meyran@tum.de>
2025-10-28 16:02:08 +0100

    fixed typos in docs

--- include/physics/GravitationalForce.h
+++ include/physics/GravitationalForce.h
@@ -7,21 +7,21 @@
 namespace mol_sim {
 
 /**
- * @brief Class to calculate gravitational forces between two different objects. Implements the ForceSource interface
+ * @brief Class to calculate gravitational forces between two different objects. Implements the ForceSource interface.
  *
  * Class to calculate gravitational forces between two different objects.
- * Implements the ForceSource interface
+ * Implements the ForceSource interface.
  */
 class GravitationalForce : public ForceSource {
  public:
   /**
-   * @brief Calculates the gravitational force a particle p2 exerts on a different particle p1
+   * @brief Calculates the gravitational force a particle p2 exerts on a different particle p1.
    *
-   * @param p1 Particle whos force is to be calculated
-   * @param p2 Particle which exerts force on p1
-   * @return Vector<double, 3> Force exerted on p1 by p2
+   * @param p1 Particle whos force is to be calculated.
+   * @param p2 Particle which exerts force on p1.
+   * @return Vector<double, 3> Force exerted on p1 by p2.
    *
-   * Calculates the gravitational force a particle p2 exerts on a different particle p1
+   * Calculates the gravitational force a particle p2 exerts on a different particle p1.
    */
   [[nodiscard]] Vector<double, 3> calculateForce(const Particle& p1, const Particle& p2) const override;
 };
