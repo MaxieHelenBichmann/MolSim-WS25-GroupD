81c8ca0b4369ab4c06cdcebcb137ae77d29c689a
Henry Meyran <henry.meyran@tum.de>
2025-10-31 21:43:54 +0100

    added CTest config

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "MolSim_GroupD")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "cdash.cmake.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=MolSim_GroupD")
+set(CTEST_DROP_SITE_CDASH_QUERY "project=MolSim_GroupD&nightly=1")
