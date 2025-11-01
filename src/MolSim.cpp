539f801cd74bbb0482fd81e3c8b6b388fbbfe0df
Henry Meyran <henry.meyran@tum.de>
2025-11-01 09:36:55 +0100

    changed file format and implement XVM reading in YAML

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -12,7 +12,7 @@ constexpr double START_TIME = 0;
 int main(int argc, char* argsv[]) {
     double delta_t = 0.014;
     double end_time = 1000;
-    XVMReader file_reader;
+    YAMLReader file_reader;
     SimpleContainer particles;
 
     cliParse(argc, argsv, file_reader, delta_t, end_time, particles);
--- src/io/fileReader/YAMLReader.cpp
+++ src/io/fileReader/YAMLReader.cpp
@@ -1,10 +1,14 @@
 #include "io/fileReader/YAMLReader.h"
 
+#include <cstddef>
 #include <cstdlib>
 #include <iostream>
 #include <string>
 
 namespace mol_sim {
+YAMLReader::YAMLReader() = default;
+
+YAMLReader::~YAMLReader() = default;
 void YAMLReader::readFile(ContainerRef particles, const std::string& filename) {
     try {
         YAML::Node root = YAML::LoadFile(filename);
@@ -24,18 +28,20 @@ void YAMLReader::readFile(ContainerRef particles, const std::string& filename) {
 void YAMLReader::readXVM(ContainerRef particles, YAML::Node& node) {
     try {
         if (node["particles"] && node["particles"].IsSequence()) {
+            auto num_part = node["num_particles"].as<size_t>();
+            particles.reserve(num_part);
             for (const auto& curr : node["particles"]) {
                 R3 position;
                 const YAML::Node& coordinates = curr["coordinates"];
-                position[0] = coordinates[0].as<double>();
-                position[1] = coordinates[1].as<double>();
-                position[2] = coordinates[2].as<double>();
+                position[0] = coordinates["x"].as<double>();
+                position[1] = coordinates["y"].as<double>();
+                position[2] = coordinates["z"].as<double>();
 
                 R3 velocity;
                 const YAML::Node& velocity_node = curr["velocity"];
-                velocity[0] = velocity_node[0].as<double>();
-                velocity[1] = velocity_node[1].as<double>();
-                velocity[2] = velocity_node[2].as<double>();
+                velocity[0] = velocity_node["vx"].as<double>();
+                velocity[1] = velocity_node["vy"].as<double>();
+                velocity[2] = velocity_node["vz"].as<double>();
 
                 auto mass = curr["mass"].as<double>();
 
@@ -47,5 +53,32 @@ void YAMLReader::readXVM(ContainerRef particles, YAML::Node& node) {
         exit(EXIT_FAILURE);
     }
 }
-void YAMLReader::readCube(ContainerRef particles, YAML::Node& node) {}
+void YAMLReader::readCube(ContainerRef particles, YAML::Node& node) {
+    try {
+        if (node["particles"] && node["particles"].IsSequence()) {
+            auto num_part = node["num_particles"].as<size_t>();
+            particles.reserve(num_part);
+            for (const auto& curr : node["particles"]) {
+                R3 position;
+                const YAML::Node& coordinates = curr["coordinates"];
+                position[0] = coordinates["x"].as<double>();
+                position[1] = coordinates["y"].as<double>();
+                position[2] = coordinates["z"].as<double>();
+
+                R3 velocity;
+                const YAML::Node& velocity_node = curr["velocity"];
+                velocity[0] = velocity_node["vx"].as<double>();
+                velocity[1] = velocity_node["vy"].as<double>();
+                velocity[2] = velocity_node["vz"].as<double>();
+
+                auto mass = curr["mass"].as<double>();
+
+                particles.addParticle(position, velocity, mass);
+            };
+        }
+    } catch (const YAML::Exception& e) {
+        std::cerr << "Error parsing YAML: " << e.what() << '\n';
+        exit(EXIT_FAILURE);
+    }
+}
 }  // namespace mol_sim
