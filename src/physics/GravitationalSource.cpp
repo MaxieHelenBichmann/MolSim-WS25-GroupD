9d53f26d742411fb30759b590f7f22784f99f022
Henry Meyran <henry.meyran@tum.de>
2025-11-02 13:15:45 +0100

    added error hnadling in force calc when p1 and p2 have same postion

--- src/physics/GravitationalSource.cpp
+++ src/physics/GravitationalSource.cpp
@@ -6,6 +6,9 @@ using namespace mol_sim;
 Vector<double, 3> GravitationalForce::calculateForce(const Particle& p1, const Particle& p2) const {
     const auto difference = p2.getX() - p1.getX();
     const double distance = difference.euclidNorm();
+    if (distance == 0) {
+        return {0.0, 0.0, 0.0};
+    }
     const auto grav_force = p1.getM() * p2.getM() / std::pow(distance, 3) * difference;
     return grav_force;
 }
