e1e3a1429828e48abce5b7af53ecabbe2e1c5877
Maxie Helen Bichmann <Maxie.Bichmann@tum.de>
2025-11-02 17:18:39 +0100

    hacky workaround for library warnings

--- cmake/modules/yaml-cpp.cmake
+++ cmake/modules/yaml-cpp.cmake
@@ -10,3 +10,6 @@ set(YAML_CPP_CLANG_FORMAT_EXE OFF
     CACHE BOOL "Force clang-format to not be found by yaml-cpp")
 FetchContent_MakeAvailable(yaml-cpp)
 set_property(DIRECTORY ${yaml-cpp_SOURCE_DIR} PROPERTY SYSTEM TRUE)
+if(TARGET yaml-cpp)
+    target_compile_options(yaml-cpp PRIVATE -Wno-shadow)
+endif()
