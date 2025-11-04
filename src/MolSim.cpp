f29de8db8f8ac5b4d46e678ea5779f2b03ea7b6b
Henry Meyran <henry.meyran@tum.de>
2025-11-04 14:31:10 +0100

    fixed compile isues

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argsv[]) {
 
     // explicit .value_or despite call to setDefaults to make warnings disappear
     SPDLOG_INFO("Simulation configured with {} particles, delta_t={} end_time={}", particles.size(),
-                settings.delta_t.value, settings.end_time.value);  // NOLINT((bugprone-unchecked-optional-access))
+                settings.delta_t.value(), settings.end_time.value());  // NOLINT((bugprone-unchecked-optional-access))
     Simulation<SimpleContainer> simulation(particles, settings);
     simulation.run();
 }
