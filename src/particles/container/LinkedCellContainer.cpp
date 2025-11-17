f252dc3601fa6606ea54a924fedf28ad2d0502fa
Maxie Helen Bichmann <Maxie.Bichmann@tum.de>
2025-11-17 16:13:35 +0100

    fix small issues

--- src/particles/container/LinkedCellContainer.cpp
+++ src/particles/container/LinkedCellContainer.cpp
@@ -13,7 +13,7 @@ LinkedCellContainer::Cell::Cell(CellType cell_type, std::array<double, 6> bounds
 
 void LinkedCellContainer::Cell::addParticle(size_t idx) { indices.push_back(idx); }
 void LinkedCellContainer::Cell::removeParticle(size_t idx) {
-    for (size_t i = 0; i < indices.size(); ++i) {
+    for (size_t i = 0; i < indices.size(); ++i) {  // NOLINT
         if (indices[i] == idx) {
             indices.erase(indices.begin() + i);  // NOLINT
             return;
@@ -21,7 +21,7 @@ void LinkedCellContainer::Cell::removeParticle(size_t idx) {
     }
 }
 void LinkedCellContainer::Cell::updateParticleIndex(size_t old_idx, size_t new_idx) {
-    for (size_t i = 0; i < indices.size(); ++i) {
+    for (size_t i = 0; i < indices.size(); ++i) {  // NOLINT
         if (indices[i] == old_idx) {
             indices[i] = new_idx;
             return;
@@ -33,8 +33,8 @@ std::vector<size_t>& LinkedCellContainer::Cell::particles() { return indices; }
 const std::vector<size_t>& LinkedCellContainer::Cell::particles() const { return indices; }
 size_t LinkedCellContainer::Cell::operator[](size_t idx) { return indices[idx]; }
 bool LinkedCellContainer::Cell::fits(R3 x) const {
-    return bounds[0] <= x[0] && x[0] < bounds[1] && bounds[2] <= x[1] && x[1] < bounds[3] && bounds[4] <= x[2] &&
-           x[2] < bounds[5];
+    return bounds[0] <= x[0] && x[0] <= bounds[1] && bounds[2] <= x[1] && x[1] <= bounds[3] && bounds[4] <= x[2] &&
+           x[2] <= bounds[5];
 }
 
 // ------------------- LinkedCellContainer methods -------------------
@@ -296,8 +296,8 @@ void LinkedCellContainer::decreaseCellIndices(size_t starting_idx) {
 }
 
 bool LinkedCellContainer::fitsDomain(R3 v) const {
-    return (v[0] >= 0.0 && v[0] < domain_size[0]) && (v[1] >= 0.0 && v[1] < domain_size[1]) &&
-           (v[2] >= 0.0 && v[2] < domain_size[2]);
+    return (v[0] >= 0.0 && v[0] <= domain_size[0]) && (v[1] >= 0.0 && v[1] <= domain_size[1]) &&
+           (v[2] >= 0.0 && v[2] <= domain_size[2]);
 }
 
 bool LinkedCellContainer::fitsContainer(R3 v) const {
