69aadeaa87a25636cfff3174611c7ba1f08a905e
Henry Meyran <henry.meyran@tum.de>
2025-11-24 14:47:15 +0100

    full benchmarks wip

--- benchmarks/src/FullBenchmarks.cpp
+++ benchmarks/src/FullBenchmarks.cpp
@@ -37,9 +37,10 @@ static void bmSimulationGiven(benchmark::State& state) {
     LennardJonesForce lj_force;
     SimpleContainer part_container;
     ContainerRef particles(part_container);
-    CuboidGenerator generator1({0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {40U, 8U, 1U}, 1.0, 1.0, 0.1, 5.0, 1.0);
-    CuboidGenerator generator2({15.0, 15.0, 0.0}, {0.0, -10.0, 0.0}, {8U, 8U, 1U}, 1.0, 1.0, 0.1, 5.0, 1.0);
-    SettingsParam settings(0.14, 0, 1000, 0.0, 1.0);
+    CuboidGenerator generator1({20.0, 0.0, 0.0}, {0., 0.0, 0.0}, {100U, 20U, 1U}, 1.0, 1.1225, 0.1, 5.0, 1.0);
+    CuboidGenerator generator2({70.0, 60.0, 0.0}, {0.0, -10.0, 0.0}, {10U, 20U, 1U}, 1.0, 1.1225, 0.1, 5.0, 1.0);
+    SettingsParam settings(0.0005, 0, 20, 5.0, 1.0, );
+
     Simulation<SimpleContainer, LennardJonesForce> simulation(part_container, lj_force, settings);
     for ([[maybe_unused]] auto _ : state) {
         generator1.generateParticles(particles);
@@ -49,5 +50,10 @@ static void bmSimulationGiven(benchmark::State& state) {
         benchmark::DoNotOptimize(particles);
     }
 }
+
+void bmSimulationGivenCutOff(benchmark::State& state) {}
+
+void bmSimulationGivenLC(benchmark::State& state) {}
+
 BENCHMARK(bmSimulationGiven)->Repetitions(10)->Unit(benchmark::kMillisecond);
 }  // namespace mol_sim
