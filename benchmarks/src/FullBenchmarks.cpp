0d572f39231f0d10b5bdad044d39ac497dd7777a
Henry Meyran <henry.meyran@tum.de>
2025-11-26 16:08:21 +0100

    benchmark wip

--- benchmarks/src/FullBenchmarks.cpp
+++ benchmarks/src/FullBenchmarks.cpp
@@ -2,13 +2,15 @@
 
 #include <memory>
 
+#include "../code/linkedcellimpl/LinkedCellContainerDirect.h"
+#include "../code/linkedcellimpl/LinkedCellContainerExplicit.h"
 #include "io/outputWriter/XYZWriter.h"
-#include "particles/container/LinkedCellContainer.h"
 #include "particles/container/SimpleContainer.h"
 #include "particles/generators/CuboidGenerator.h"
 #include "physics/LennardJonesForce.h"
 #include "utils/Settings.h"
 #include "utils/Simulation.h"
+
 namespace mol_sim {
 /**
  * @brief Tests the simulation with the parameters given in Assignment 2
@@ -56,8 +58,8 @@ void bmSimulationGivenCutOff(benchmark::State& state) {
     }
 }
 
-void bmSimulationGivenLC(benchmark::State& state) {
-    LinkedCellContainer part_container({180., 90., 1.}, 3.0);
+void bmSimulationGivenLCDirect(benchmark::State& state) {
+    LinkedCellContainerDirect part_container({180., 90., 1.}, 3.0);
     ContainerRef particles(part_container);
     CuboidGenerator generator1({20.0, 0.0, 0.0}, {0., 0.0, 0.0}, {100U, 20U, 1U}, 1.0, 1.1225, 0.1, 5.0, 1.0);
     CuboidGenerator generator2({70.0, 60.0, 0.0}, {0.0, -10.0, 0.0}, {20U, 20U, 1U}, 1.0, 1.1225, 0.1, 5.0, 1.0);
@@ -67,7 +69,8 @@ void bmSimulationGivenLC(benchmark::State& state) {
     settings.setDefaults();
     auto force_source = std::make_unique<LennardJonesForce>();
     auto writer = std::make_unique<XYZWriter>();
-    Simulation<LinkedCellContainer> simulation(part_container, std::move(force_source), settings, std::move(writer));
+    Simulation<LinkedCellContainerDirect> simulation(part_container, std::move(force_source), settings,
+                                                     std::move(writer));
     for ([[maybe_unused]] auto _ : state) {
         generator1.generateParticles(particles);
         generator2.generateParticles(particles);
@@ -77,40 +80,22 @@ void bmSimulationGivenLC(benchmark::State& state) {
     }
 }
 
-void bmSimulationSingleLC(benchmark::State& state) {
-    size_t n = state.range(0);
-    LinkedCellContainer part_container({180., 90., 1.}, 3.0);
+void bmSimulationGivenLCExplicit(benchmark::State& state) {
+    LinkedCellContainerExplicit part_container({180., 90., 1.}, 3.0);
     ContainerRef particles(part_container);
-
-    CuboidGenerator generator({60.0, 60.0, 0.0}, {0.0, -10.0, 0.0}, {n, n, n}, 1.0, 1.1225, 0.1, 5.0, 1.0);
-    SettingsParam settings(0.0005, 0, 0.0005, 5.0, 1.0);
+    CuboidGenerator generator1({20.0, 0.0, 0.0}, {0., 0.0, 0.0}, {100U, 20U, 1U}, 1.0, 1.1225, 0.1, 5.0, 1.0);
+    CuboidGenerator generator2({70.0, 60.0, 0.0}, {0.0, -10.0, 0.0}, {20U, 20U, 1U}, 1.0, 1.1225, 0.1, 5.0, 1.0);
+    SettingsParam settings(0.0005, 0, 20, 5.0, 1.0);
     settings.cutoff = 3.0;
     settings.domain = {.dimension = {180.0, 90., 1.}};
     settings.setDefaults();
     auto force_source = std::make_unique<LennardJonesForce>();
     auto writer = std::make_unique<XYZWriter>();
-    generator.generateParticles(particles);
-
-    Simulation<LinkedCellContainer> simulation(part_container, std::move(force_source), settings, std::move(writer));
+    Simulation<LinkedCellContainerExplicit> simulation(part_container, std::move(force_source), settings,
+                                                       std::move(writer));
     for ([[maybe_unused]] auto _ : state) {
-        benchmark::ClobberMemory();
-        simulation.run();
-        benchmark::DoNotOptimize(particles);
-    }
-}
-void bmSimulationSingleDirSum(benchmark::State& state) {
-    size_t n = state.range(0);
-    SimpleContainer part_container;
-    ContainerRef particles(part_container);
-    CuboidGenerator generator({60.0, 60.0, 0.0}, {0., 0., 0.}, {n, n, n}, 1.0, 1.1225, 0.1, 5.0, 1.0);
-    SettingsParam settings(0.0005, 0, 0.0005, 5.0, 1.0);
-    settings.domain = {.dimension = {180.0, 90., 1.}};
-    settings.setDefaults();
-    auto force_source = std::make_unique<LennardJonesForce>();
-    auto writer = std::make_unique<XYZWriter>();
-    generator.generateParticles(particles);
-    Simulation<SimpleContainer> simulation(part_container, std::move(force_source), settings, std::move(writer));
-    for ([[maybe_unused]] auto _ : state) {
+        generator1.generateParticles(particles);
+        generator2.generateParticles(particles);
         benchmark::ClobberMemory();
         simulation.run();
         benchmark::DoNotOptimize(particles);
@@ -121,20 +106,7 @@ BENCHMARK(bmSimulationGiven)->Unit(benchmark::kMillisecond)->Repetitions(5)->Dis
 
 BENCHMARK(bmSimulationGivenCutOff)->Unit(benchmark::kMillisecond)->Repetitions(5)->DisplayAggregatesOnly(true);
 
-BENCHMARK(bmSimulationGivenLC)->Unit(benchmark::kMillisecond)->Repetitions(5)->DisplayAggregatesOnly(true);
-
-BENCHMARK(bmSimulationSingleLC)
-    ->DenseRange(10, 20, 2)
-    ->Unit(benchmark::kMillisecond)
-    ->Complexity()
-    ->Repetitions(5)
-    ->DisplayAggregatesOnly(true);
-
-BENCHMARK(bmSimulationSingleDirSum)
-    ->DenseRange(10, 20, 2)
-    ->Unit(benchmark::kMillisecond)
-    ->Complexity()
-    ->Repetitions(5)
-    ->DisplayAggregatesOnly(true);
+BENCHMARK(bmSimulationGivenLCDirect)->Unit(benchmark::kMillisecond)->Repetitions(5)->DisplayAggregatesOnly(true);
+BENCHMARK(bmSimulationGivenLCExplcit)->Unit(benchmark::kMillisecond)->Repetitions(5)->DisplayAggregatesOnly(true);
 
 }  // namespace mol_sim
---
+++ benchmarks/src/StepWiseBenchmarks.cpp
@@ -0,0 +1,67 @@
+#include <benchmark/benchmark.h>
+
+#include <memory>
+
+#include "io/outputWriter/XYZWriter.h"
+#include "particles/container/LinkedCellContainer.h"
+#include "particles/container/SimpleContainer.h"
+#include "particles/generators/CuboidGenerator.h"
+#include "physics/LennardJonesForce.h"
+#include "utils/Settings.h"
+#include "utils/Simulation.h"
+
+namespace mol_sim {
+
+void bmSimulationSingleLC(benchmark::State& state) {
+    size_t n = state.range(0);
+    LinkedCellContainer part_container({180., 90., 1.}, 3.0);
+    ContainerRef particles(part_container);
+
+    CuboidGenerator generator({60.0, 60.0, 0.0}, {0.0, -10.0, 0.0}, {n, n, n}, 1.0, 1.1225, 0.1, 5.0, 1.0);
+    SettingsParam settings(0.0005, 0, 0.0005, 5.0, 1.0);
+    settings.cutoff = 3.0;
+    settings.domain = {.dimension = {180.0, 90., 1.}};
+    settings.setDefaults();
+    auto force_source = std::make_unique<LennardJonesForce>();
+    auto writer = std::make_unique<XYZWriter>();
+    generator.generateParticles(particles);
+
+    Simulation<LinkedCellContainer> simulation(part_container, std::move(force_source), settings, std::move(writer));
+    for ([[maybe_unused]] auto _ : state) {
+        benchmark::ClobberMemory();
+        simulation.run();
+        benchmark::DoNotOptimize(particles);
+    }
+}
+void bmSimulationSingleDirSum(benchmark::State& state) {
+    size_t n = state.range(0);
+    SimpleContainer part_container;
+    ContainerRef particles(part_container);
+    CuboidGenerator generator({60.0, 60.0, 0.0}, {0., 0., 0.}, {n, n, n}, 1.0, 1.1225, 0.1, 5.0, 1.0);
+    SettingsParam settings(0.0005, 0, 0.0005, 5.0, 1.0);
+    settings.domain = {.dimension = {180.0, 90., 1.}};
+    settings.setDefaults();
+    auto force_source = std::make_unique<LennardJonesForce>();
+    auto writer = std::make_unique<XYZWriter>();
+    generator.generateParticles(particles);
+    Simulation<SimpleContainer> simulation(part_container, std::move(force_source), settings, std::move(writer));
+    for ([[maybe_unused]] auto _ : state) {
+        benchmark::ClobberMemory();
+        simulation.run();
+        benchmark::DoNotOptimize(particles);
+    }
+}
+BENCHMARK(bmSimulationSingleLC)
+    ->DenseRange(10, 20, 2)
+    ->Unit(benchmark::kMillisecond)
+    ->Complexity()
+    ->Repetitions(5)
+    ->DisplayAggregatesOnly(true);
+
+BENCHMARK(bmSimulationSingleDirSum)
+    ->DenseRange(10, 20, 2)
+    ->Unit(benchmark::kMillisecond)
+    ->Complexity()
+    ->Repetitions(5)
+    ->DisplayAggregatesOnly(true);
+}  // namespace mol_sim
