da4df922bd20d0bc7f0ad618b7380ac6dc68163b
Maxie Helen Bichmann <Maxie.Bichmann@tum.de>
2025-12-03 13:43:27 +0100

    update docs container

--- include/particles/container/ContainerRef.h
+++ include/particles/container/ContainerRef.h
@@ -324,7 +324,8 @@ class ContainerRef {
     static_assert(std::forward_iterator<proximity_iterator>);
 
     /**
-     * @brief Remove a Particle.
+     * @brief Remove a Particle. Used when iterating with proximity_iterator, not used yet
+     * (thus not tested), but could be useful.
      *
      * @param p Iterator to to the Particle to remove.
      *
