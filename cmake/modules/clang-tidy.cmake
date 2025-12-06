3ec9dfeb1b7f2e90a9866f4e09277e5e9801909d
Maxie Helen Bichmann <Maxie.Bichmann@tum.de>
2025-12-06 20:38:00 +0100

    improve test infrastructure for containers

--- cmake/modules/clang-tidy.cmake
+++ cmake/modules/clang-tidy.cmake
@@ -1,36 +1,32 @@
-option(ENABLE_CLANG_TIDY "Enable clang-tidy checks" ON)
+message(STATUS "clang-tidy checks enabled")
 
-if(ENABLE_CLANG_TIDY)
-    message(STATUS "clang-tidy checks enabled")
-    
-    find_program(CLANG_TIDY_EXE clang-tidy)
-    if(CLANG_TIDY_EXE)
-        #For other options like automatic fixes add --fix at the end
-        set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} -p ${CMAKE_BINARY_DIR})
-        message(STATUS "clang-tidy found and enabled: ${CLANG_TIDY_EXE}")
+find_program(CLANG_TIDY_EXE clang-tidy)
+if(CLANG_TIDY_EXE)
+    #For other options like automatic fixes add --fix at the end
+    set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} -p ${CMAKE_BINARY_DIR})
+    message(STATUS "clang-tidy found and enabled: ${CLANG_TIDY_EXE}")
 
-        file(GLOB_RECURSE ALL_CXX_SOURCES CONFIGURE_DEPENDS
-            "${CMAKE_SOURCE_DIR}/src/*.cpp"
-            "${CMAKE_SOURCE_DIR}/include/*.h"
-            "${CMAKE_SOURCE_DIR}/tests/*.cpp"
-            "${CMAKE_SOURCE_DIR}/benchmarks/*.cpp"
-            "${CMAKE_SOURCE_DIR}/benchmarks/*.h)"
+    file(GLOB_RECURSE ALL_CXX_SOURCES CONFIGURE_DEPENDS
+        "${CMAKE_SOURCE_DIR}/src/*.cpp"
+        "${CMAKE_SOURCE_DIR}/include/*.h"
+        "${CMAKE_SOURCE_DIR}/tests/*.cpp"
+        "${CMAKE_SOURCE_DIR}/benchmarks/*.cpp"
+        "${CMAKE_SOURCE_DIR}/benchmarks/*.h)"
 
-        )
+    )
 
-        add_custom_target(
-            fix
-            COMMAND ${CLANG_TIDY_EXE}
-            --fix
-            --fix-errors
-            --fix-notes
-            -p=${CMAKE_BINARY_DIR}
-            --config-file=${CMAKE_SOURCE_DIR}/.clang-tidy
-            ${ALL_CXX_SOURCES}
-            COMMENT "Running clang-tidy with fixes"
-        )
-        message(STATUS "clang-tidy fix target created. Build it with 'make fix'.")
-    else ()
-        message(WARNING "clang-tidy not found, no static analysis")
-    endif ()
-endif()
+    add_custom_target(
+        fix
+        COMMAND ${CLANG_TIDY_EXE}
+        --fix
+        --fix-errors
+        --fix-notes
+        -p=${CMAKE_BINARY_DIR}
+        --config-file=${CMAKE_SOURCE_DIR}/.clang-tidy
+        ${ALL_CXX_SOURCES}
+        COMMENT "Running clang-tidy with fixes"
+    )
+    message(STATUS "clang-tidy fix target created. Build it with 'make fix'.")
+else ()
+    message(WARNING "clang-tidy not found, no static analysis")
+endif ()
