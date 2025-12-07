9fdb01a9de61fce59dcc1b7a9e6576ccd3b17eef
Maxie Helen Bichmann <Maxie.Bichmann@tum.de>
2025-12-07 21:28:27 +0100

    add tests for YAMLWriterCP

--- tests/io/XVMWriterCP_test.cpp
+++ tests/io/XVMWriterCP_test.cpp
@@ -44,8 +44,8 @@ class XVMWriterCPTest : public testing::Test {
      */
     std::filesystem::path writeCheckpoint(SimpleContainer& container, int iteration, size_t N) {
         ContainerRef particles(container);
-        writer.createCheckpoint(domain, particles, iteration, Force::GRAVITATIONAL, 0.01, 0.0, 1.0, 1, 1, "base", 1.0,
-                                N);
+        writer.createCheckpoint(domain, particles, iteration, Force::GRAVITATIONAL, 0.01, 0.0, 1.0, 1, 1, "test_xvm",
+                                1.0, N);
 
         int decimal_places = 0;
         while (N >= 10) {
