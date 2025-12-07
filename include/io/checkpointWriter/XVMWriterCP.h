12e136f69fc33496e5f00c1ea59e8fc905c88615
Maxie Helen Bichmann <Maxie.Bichmann@tum.de>
2025-12-07 12:51:51 +0100

    dumb down XVMWriterCP to be compatible with XVMReader

--- include/io/checkpointWriter/XVMWriterCP.h
+++ include/io/checkpointWriter/XVMWriterCP.h
@@ -7,8 +7,10 @@
 namespace mol_sim {
 
 /**
- * @brief Writes all particle data into a .txt file, but misses other state of a simulation.
- * Only included for backward compatibility with the XVMReader.
+ * @brief Writes all particle data into a .txt file that can be read by the XVMReader, but misses other state of a
+simulation (as the XVM format does not support this). Also misses certain components of the particle data, like
+Forces, Sigma, Epsilon or type.
+ * Basically very useless and only included for backward compatibility with the XVMReader.
  *
  * The filename will consist of cp_<iteration>.txt
  */
--- src/io/checkpointWriter/XVMWriterCP.cpp
+++ src/io/checkpointWriter/XVMWriterCP.cpp
@@ -4,9 +4,6 @@
 #include <fstream>
 #include <iomanip>
 #include <sstream>
-#include <string_view>
-
-#include "particles/container/SimpleContainer.h"
 
 using namespace mol_sim;
 
@@ -30,71 +27,16 @@ void XVMWriterCP::createCheckpoint([[maybe_unused]] const Domain& domain, Contai
     std::stringstream strstr;
     strstr << "cp_" << std::setfill('0') << std::setw(decimal_places) << iteration << ".txt";
 
-    // Settings
     file.open(strstr.str().c_str());
-    file << "# Domain" << '\n';
-    file << domain.getDimension()[0] << " " << domain.getDimension()[1] << " " << domain.getDimension()[2] << '\n';
-
-    const auto boundary_type_string = [](BoundaryType type) -> std::string_view {
-        switch (type) {
-            case BoundaryType::OUTFLOW:
-                return "OUTFLOW";
-            case BoundaryType::REFLECTING:
-                return "REFLECTING";
-            case BoundaryType::VELOCITYREFLECT:
-                return "VELOCITYREFLECT";
-        }
-        return "OUTFLOW";
-    };
-
-    file << "# Boundaries" << '\n';
-    file << "upper " << boundary_type_string(domain.getBoundary(BoundaryLocation::UPPER).getType()) << '\n';
-    file << "lower " << boundary_type_string(domain.getBoundary(BoundaryLocation::LOWER).getType()) << '\n';
-    file << "left " << boundary_type_string(domain.getBoundary(BoundaryLocation::LEFT).getType()) << '\n';
-    file << "right " << boundary_type_string(domain.getBoundary(BoundaryLocation::RIGHT).getType()) << '\n';
-    file << "front " << boundary_type_string(domain.getBoundary(BoundaryLocation::FRONT).getType()) << '\n';
-    file << "back " << boundary_type_string(domain.getBoundary(BoundaryLocation::BACK).getType()) << '\n';
-
-    file << "# Force" << '\n';
-
-    file << "# delta_t" << '\n';
-    file << delta_t << '\n';
-
-    file << "# start_time" << '\n';
-    file << start_time << '\n';
-
-    file << "# end_time" << '\n';
-    file << end_time << '\n';
-
-    file << "# frequency_output" << '\n';
-    file << frequency_output << '\n';
-
-    file << "# frequency_checkpoint" << '\n';
-    file << frequency_checkpoint << '\n';
-
-    file << "# base_name" << '\n';
-    file << base_name << '\n';
-
-    file << "# cutoff_radius" << '\n';
-    file << cutoff_radius << '\n';
 
     // Particles
-    file << "# Container Type" << '\n';
-    file << (std::holds_alternative<SimpleContainer*>(particles.getInstance()) ? "SIMPLE" : "LINKED") << '\n';
-    file << "# Particles" << '\n';
+    file << "# Number Particles" << '\n';
     file << particles.size() << '\n';
-    file << "# Position | Velocity | Force | Old Force | Mass | Sigma | Epsilon | Type " << '\n';
+    file << "# Position | Velocity | Mass" << '\n';
     for (auto& p : particles) {
         file << p.getX()[0] << " " << p.getX()[1] << " " << p.getX()[2] << " ";
         file << p.getV()[0] << " " << p.getV()[1] << " " << p.getV()[2] << " ";
-        file << p.getF()[0] << " " << p.getF()[1] << " " << p.getF()[2] << " ";
-        file << p.getOldF()[0] << " " << p.getOldF()[1] << " " << p.getOldF()[2] << " ";
-        file << p.getM() << " ";
-        file << p.getSigma() << " ";
-        file << p.getEpsilon() << " ";
-        file << p.getType();
-        file << '\n';
+        file << p.getM() << '\n';
     }
-
     file.close();
 }
