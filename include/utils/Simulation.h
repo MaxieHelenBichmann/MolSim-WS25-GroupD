91970de97603fd723c36bf64784604fea000322e
Maxie Helen Bichmann <Maxie.Bichmann@tum.de>
2025-12-10 20:26:02 +0100

    add compile option (make toggle-able) for io and cp

--- include/utils/Simulation.h
+++ include/utils/Simulation.h
@@ -297,7 +297,7 @@ class Simulation {
             calculateV(thermo_factor);
 
             iteration++;
-#ifndef DISABLE_IO
+#ifdef ENABLE_IO
             if (iteration % frequency_output == 0) {
                 try {
                     std::string out_name = base_name;
@@ -308,7 +308,7 @@ class Simulation {
                 }
             }
 #endif
-#ifndef DISABLE_CHECKPOINTING
+#ifdef ENABLE_CHECKPOINTING
             if (iteration % frequency_checkpoint == 0) {
                 try {
                     cp_writer.createCheckpoint(domain, particles, iteration, force, delta_t, start_time, end_time,
