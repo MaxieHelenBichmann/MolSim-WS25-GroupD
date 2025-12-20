cbb9e0b93563a11ec21e00a6d7ec3435dcb3b9c0
Georg Eisner <gwagwa@Mac.fritz.box>
2025-12-20 23:23:27 +0100

    dummy commit

--- include/particles/boundaries/Periodic.h
+++ include/particles/boundaries/Periodic.h
@@ -5,22 +5,11 @@
 
 #include "particles/boundaries/Boundary.h"
 #include "particles/container/LinkedCellContainer.h"
+#include "utils/Vector.h"
 
 namespace mol_sim {
 
 class Periodic : public Boundary {
-    /**
-     * @brief Used to check if a particle lies within the halo of the domain.
-     * 
-     * @note If domain_size is not divisible by the cutoff radius in at least one dimension
-     * doing it this way will flag some particles as "in the halo" of a SimpleContainer
-     * even though they're not. This is additional overhead that can (and should) be eliminated
-     * (especially if it creates a bunch of overhead). In the interest of readability and avoiding
-     * more weird branches (if container == SimpleContainer...) and entanglement I decided to 
-     * do it like this.
-     */
-    static std::unique_ptr<LinkedCellContainer> checker;
-    
     /**
      * @brief The size of the corners in the domain. This is needed so the corners are copied correctly
      *
@@ -29,24 +18,40 @@ class Periodic : public Boundary {
      */
     std::array<double, 3> halo_dimension;
 
-    bool is2D = false; //NOLINT
+    static constexpr const std::array<uint8_t, 62> mirrorIdx_lookup = {
+        //-----------------------------3D--------------------------------
+        25, 17,  7, 15, 26,  8,  6, 24, //LEFT
+        19, 11,  1,  9, 20,  2,  0, 18, //RIGHT
+        25, 23, 19, 21, 26, 20, 18, 24, //FRONT
+         7,  5,  1,  3,  8,  2,  0,  6, //BACK
+        15, 21,  9,  3, 24, 18,  0,  6, //UPPER
+        17, 23, 11,  5, 26, 20,  2,  8, //LOWER
+        16, 10, 22, 4, 12, 15, //non-corner / edges mirror indicies in order: LEFT, RIGHT, FRONT, BACK, UPPER, LOWER
+        
+        //-----------------------------2D--------------------------------
+        25, 19, 1, 7, //2D corners in order: lower-left, lower-right, upper-right, upper-left
+        16, 10, 22, 4 //2D non-corner mirror indicies in order: LEFT, RIGHT, FRONT, BACK
+    };
 
     /**
-     * @brief Returns true if the particle is in a corner of the simulation domain, false otherwise.
-     * 
-     * @param p The particle to be examined.
-     * @return true if the particle is in a corner of the simulation domain.
-     * @return false otherwise.
+     * TODO: make this static const
      */
-    [[nodiscard]] bool isInHalo(R3 x) const noexcept;
+    //z inc -> x inc -> y inc
+    std::array<R3, 27> shift_lookup;
 
-    [[nodiscard]] bool isOnBoundary(R3 x, size_t axis, int sign) const noexcept;
+    bool is2D = false; //NOLINT
 
     void teleportParticleIfOOB(Particle& p);
+    
+    [[nodiscard]] bool isOnBoundary(R3 x, size_t axis, int sign) const noexcept;
+    
+    [[nodiscard]] std::vector<Particle> mirrorParticle(Particle& p); 
 
-    std::vector<Particle> mirrorParticle(Particle& p);
+    [[nodiscard]] size_t getIdx(Particle& p);
+    
+    void addMirrorParticle(const R3& mirrorLocation, size_t mirrorIdx, Particle& p, std::vector<Particle>& mirrored_particles);
 
-    void updateOffset(R3& offset, size_t i);
+    void setShiftLookup(R3 domain_size);
 
    public:
     Periodic(BoundaryLocation location, R3 domain_size, double cutoff, bool is2D) noexcept;
@@ -59,7 +64,7 @@ class Periodic : public Boundary {
      * Particles that are:
      * - in the interior of the domain:        will be ignored
      * - in the boundary region of the domain: will be mirrored
-     * - exactly on a boundary:                will be mirrored
+     * - exactly on a boundary:                will be mirrored (IDK!!!)
      * - in the halo region of the domain:     will be teleported
      * - beyond the halo region of the domain: will be ignored
      * 
