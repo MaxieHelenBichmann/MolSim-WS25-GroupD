072fd70a1e7c73cecb4223f468441881a7e65ebd
Henry Meyran <henry.meyran@tum.de>
2026-01-18 18:41:27 +0100

    fixes

--- benchmarks/code/boundaryimpl/PeriodicOld.cpp
+++ benchmarks/code/boundaryimpl/PeriodicOld.cpp
@@ -45,13 +45,9 @@ void PeriodicOld::applyBoundary(  // NOLINT
     Particle& p, [[maybe_unused]] const PairwiseForceSource& force) noexcept {
     teleportParticleIfOOB(p);
     if (!isOnBoundary(p.getX(), getAxis(), getSign())) {  // only mirror particles in correct boundary region
-        return std::nullopt;
+        return;
     }
-    auto mirrored_particles = mirrorParticle(p);
-    if (mirrored_particles.empty()) {
-        return std::nullopt;
-    }
-    return mirrored_particles;
+    mirrorParticle(p);
 }
 
 void PeriodicOld::teleportParticleIfOOB(Particle& p) {
@@ -89,9 +85,7 @@ std::vector<Particle> PeriodicOld::mirrorParticle(Particle& p) {
         // add mirror particle if necessary
         if (i != 13 && isInHalo(p.getX() + offset) && (p.getMirrorLocations() & (1 << i)) == 0) {
             Particle p_prime(p);
-            p_prime.getX() = p.getX() + offset;
-            p_prime.getType() = 1;
-            mirrored_particles.push_back(p_prime);
+            p.getMirrorPositions().emplace_back(p.getX() + offset);
             p.getMirrorLocations() |= (1 << i);
         }
 
